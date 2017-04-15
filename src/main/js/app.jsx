5b0d9d95b6a2ecc7f64930efaeabc165a6038d2a
Dave Clay <daveclay@gmail.com>
2017-04-15 07:16:48 -0700

    Making React work.

--- src/main/js/app.jsx
+++ src/main/js/app.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class Hello extends React.Component {
   render() {
     return <h1>Hello</h1>
   }
+}
+
+if (typeof document !== 'undefined') {
+  ReactDOM.render(
+    <Hello />,
+    document.getElementById('root')
+  );
+} else {
+  console.log(greeting);
 }
